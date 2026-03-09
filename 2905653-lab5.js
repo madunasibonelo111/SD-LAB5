5adf3d0343ff36377b413344ab454f59156deb23
Kgethego-M <kgethim25.o@gmail.com>
2026-03-09 14:36:40 +0200

    Part 1 and Part 2 done

---
+++ 2905653-lab5.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Your routes here
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+let books = [];
